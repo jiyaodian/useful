439a1c1d58973dd265af708312f9e98b5f2e122e
jiyaodian <jiyaodian@gmail.com>
2015-08-07 11:16:41 +0800

    update

---
+++ linux/.init.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#------------------------------------------------------------------------------
+# Color definitions
+#------------------------------------------------------------------------------
+export black='\e[0;30m'
+export BLACK='\e[1;30m'
+export red='\e[0;31m'
+export RED='\e[1;31m'
+export green='\e[0;32m'
+export GREEN='\e[1;32m'
+export yellow='\e[0;33m'
+export YELLOW='\e[1;33m'
+export blue='\e[0;34m'
+export BLUE='\e[1;34m'
+export purple='\e[0;35m'
+export PURPLE='\e[1;35m'
+export cyan='\e[0;36m'
+export CYAN='\e[1;36m'
+export NC='\e[0m'          # 无颜色
+
+# cd and ls
+function cd {
+    builtin cd "$@" && ls
+}
+
+# 修改命令行提示符样式
+PS1="[\[${CYAN}\]\u\[${NC}\]@\[${GREEN}\]\H\[${NC}\] \[${YELLOW}\]\W\[${NC}\]]\$ "
