7371455976550d48ddd43c6d3bd1bd2ebc254fd4
jiyaodian <jiyaodian@gmail.com>
2015-10-14 11:02:06 +0800

    update

--- linux/init.sh
+++ linux/init.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-[[ -s "$DIR/alias" ]] && source "$DIR/alias"
+[[ -s alias ]] && source alias
 
 #------------------------------------------------------------------------------
 # Color definitions
